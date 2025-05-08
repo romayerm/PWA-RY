aeb531a7404cb75934debb6d73b370f02eb142ba
romayerm <kseniya.yermakova97@bcmail.cuny.edu>
2025-05-07 23:03:02 -0400

    .

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let data = [];
 let currentIndex = 0;
+let currentAudio = null;
 
 const img = document.getElementById("soundImage");
 const title = document.getElementById("soundTitle");
 const desc = document.getElementById("description");
 const playButton = document.getElementById("playButton");
+const stopButton = document.getElementById("stopButton");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 
+function stopAudio() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentAudio = null;
+  }
+}
+
 function updateDisplay() {
+  stopAudio();
   const item = data[currentIndex];
   img.src = item.image;
   title.innerText = item.title;
@@ -16,9 +27,12 @@ function updateDisplay() {
 }
 
 playButton.onclick = () => {
-  const audio = new Audio(data[currentIndex].audio);
-  audio.play();
+  stopAudio();
+  currentAudio = new Audio(data[currentIndex].audio);
+  currentAudio.play();
 };
+
+stopButton.onclick = stopAudio;
 
 prevBtn.onclick = () => {
   currentIndex = (currentIndex - 1 + data.length) % data.length;
@@ -30,7 +44,7 @@ nextBtn.onclick = () => {
   updateDisplay();
 };
 
-// Fetch data from topics.json and initialize
+
 fetch("topics.json")
   .then(response => response.json())
   .then(json => {
